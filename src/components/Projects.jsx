e9aa6b71c8445991770a592b4ffb2ae038ae7426
Adebimpe Adetomiwa <adebimpeadetomiwa22@gmail.com>
2024-07-16 16:20:43 -0700

    add calculator app link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
             name: 'Windows 10 Calculator Lite',
             description:
                 'The Windows 10 Calculator Clone Lite is a simplified version of the classic Windows 10 Calculator app. This project combines clean design with essential functionality to deliver a user-friendly, interactive calculator experience.This project was an opportunity to challenge myself with a classic app redesign, focusing on essential features and clean design principles. It allowed me to explore the balance between simplicity and functionality in web development.',
-            link: '',
+            link: 'https://adebimpe-adetomiwa-22.github.io/windows-10-calculator-lite/',
             image: calculatorImage,
         },
         {
